c729271838f64921abb1761e43a4e966a697c91e
hp <534541857@qq.com>
2024-01-30 19:04:11 +0800

    perf(_Date): format 统一到 toString 中定义，microsecond，精简注释

--- src/base/_Date.js
+++ src/base/_Date.js
@@ -1,20 +1,6 @@
 // 日期时间
 export class _Date extends Date {
-  /**
-   * static
-   */
-  // [新增]
-  static REGEX_PARSE = /^(\d{4})[-/]?(\d{1,2})?[-/]?(\d{0,2})[Tt\s]*(\d{1,2})?:?(\d{1,2})?:?(\d{1,2})?[.:]?(\d+)?$/;
-  static REGEX_FORMAT = /\[([^\]]+)]|Y{1,4}|M{1,4}|D{1,2}|d{1,4}|H{1,2}|h{1,2}|a|A|m{1,2}|s{1,2}|Z{1,2}|SSS/g;
-  // static now [继承]
-  // static parse [继承]
-  // static UTC [继承]
-
-  /**
-   * constructor
-   */
   constructor(...args) {
-    // console.log('_Date constructor', args);
     if (args.length === 1) {
       // null 和显式 undefined 都视为无效值
       if (args[0] === null) {
@@ -27,7 +13,6 @@ export class _Date extends Date {
     }
     super(...args);
 
-    // [新增]
     // 这里惰性求值，不使用如 this.year=xx 的静态方式
     Object.defineProperty(this, 'year', {
       get() {
@@ -36,7 +21,7 @@ export class _Date extends Date {
     });
     Object.defineProperty(this, 'isLeapYear', {
       get() {
-        return this.year % 4 === 0 && this.year % 100 !== 0 || this.year % 400 === 0;
+        return (this.year % 4 === 0 && this.year % 100 !== 0) || (this.year % 400 === 0);
       },
     });
     Object.defineProperty(this, 'month', {
@@ -80,207 +65,20 @@ export class _Date extends Date {
         return this.getMilliseconds();
       },
     });
+    Object.defineProperty(this, 'microsecond', {
+      get() {
+        return this.getMilliseconds() * 1000;
+      },
+    });
     Object.defineProperty(this, 'timeZoneOffsetHour', {
       get() {
         return this.getTimezoneOffset() / 60;
       },
     });
-
-    // 格式化字符串用。总体同 element 用的 day.js 格式(https://day.js.org/docs/zh-CN/display/format)，风格定制成中文
-    this.format = Object.create(null);
-    const $this = this;
-    Object.defineProperty(this.format, 'YY', {
-      get() {
-        if (!$this.toBoolean()) {
-          return '';
-        }
-        return $this.year.toString().slice(-2);
-      },
-    });
-    Object.defineProperty(this.format, 'YYYY', {
-      get() {
-        if (!$this.toBoolean()) {
-          return '';
-        }
-        return $this.year.toString().slice(-4);
-      },
-    });
-    Object.defineProperty(this.format, 'M', {
-      get() {
-        if (!$this.toBoolean()) {
-          return '';
-        }
-        return $this.month.toString();
-      },
-    });
-    Object.defineProperty(this.format, 'MM', {
-      get() {
-        if (!$this.toBoolean()) {
-          return '';
-        }
-        return $this.month.toString().padStart(2, '0');
-      },
-    });
-    Object.defineProperty(this.format, 'D', {
-      get() {
-        if (!$this.toBoolean()) {
-          return '';
-        }
-        return $this.day.toString();
-      },
-    });
-    Object.defineProperty(this.format, 'DD', {
-      get() {
-        if (!$this.toBoolean()) {
-          return '';
-        }
-        return $this.day.toString().padStart(2, '0');
-      },
-    });
-    Object.defineProperty(this.format, 'd', {
-      get() {
-        if (!$this.toBoolean()) {
-          return '';
-        }
-        return ['周日', '周一', '周二', '周三', '周四', '周五', '周六'][$this.week];
-      },
-    });
-    Object.defineProperty(this.format, 'dd', {
-      get() {
-        if (!$this.toBoolean()) {
-          return '';
-        }
-        return ['星期日', '星期一', '星期二', '星期三', '星期四', '星期五', '星期六'][$this.week];
-      },
-    });
-    Object.defineProperty(this.format, 'H', {
-      get() {
-        if (!$this.toBoolean()) {
-          return '';
-        }
-        return $this.hour.toString();
-      },
-    });
-    Object.defineProperty(this.format, 'HH', {
-      get() {
-        if (!$this.toBoolean()) {
-          return '';
-        }
-        return $this.hour.toString().padStart(2, '0');
-      },
-    });
-    Object.defineProperty(this.format, 'h', {
-      get() {
-        if (!$this.toBoolean()) {
-          return '';
-        }
-        return $this.shortHour.toString();
-      },
-    });
-    Object.defineProperty(this.format, 'hh', {
-      get() {
-        if (!$this.toBoolean()) {
-          return '';
-        }
-        return $this.shortHour.toString().padStart(2, '0');
-      },
-    });
-    Object.defineProperty(this.format, 'm', {
-      get() {
-        if (!$this.toBoolean()) {
-          return '';
-        }
-        return $this.minute.toString();
-      },
-    });
-    Object.defineProperty(this.format, 'mm', {
-      get() {
-        if (!$this.toBoolean()) {
-          return '';
-        }
-        return $this.minute.toString().padStart(2, '0');
-      },
-    });
-    Object.defineProperty(this.format, 's', {
-      get() {
-        if (!$this.toBoolean()) {
-          return '';
-        }
-        return $this.second.toString();
-      },
-    });
-    Object.defineProperty(this.format, 'ss', {
-      get() {
-        if (!$this.toBoolean()) {
-          return '';
-        }
-        return $this.second.toString().padStart(2, '0');
-      },
-    });
-    Object.defineProperty(this.format, 'SSS', {
-      get() {
-        if (!$this.toBoolean()) {
-          return '';
-        }
-        return $this.millisecond.toString().padStart(3, '0');
-      },
-    });
-    Object.defineProperty(this.format, 'a', {
-      get() {
-        if (!$this.toBoolean()) {
-          return '';
-        }
-        return $this.hour < 12 ? '上午' : '下午';
-      },
-    });
-    Object.defineProperty(this.format, 'A', {
-      get() {
-        return this.a;
-      },
-    });
-    Object.defineProperty(this.format, 'Z', {
-      get() {
-        if (!$this.toBoolean()) {
-          return '';
-        }
-        const sign = $this.timeZoneOffsetHour < 0 ? '+' : '-';
-        return `${sign}${(-$this.timeZoneOffsetHour).toString().padStart(2, '0')}:00`;
-      },
-    });
-    Object.defineProperty(this.format, 'ZZ', {
-      get() {
-        return this.Z.replace(':', '');
-      },
-    });
   }
 
   /**
-   * [继承] get 系列方法。使用 year、month 等通过新增属性获取即可，简化写法，无需额外定制
-   */
-  // getTime [继承]
-  // getTimezoneOffset [继承]
-
-  // getYear [继承]
-  // getFullYear [继承]
-  // getMonth [继承]
-  // getDate [继承]
-  // getDay [继承]
-  // getHours [继承]
-  // getMinutes [继承]
-  // getSeconds [继承]
-  // getMilliseconds [继承]
-
-  // getUTCFullYear [继承]
-  // getUTCMonth [继承]
-  // getUTCDate [继承]
-  // getUTCDay [继承]
-  // getUTCHours [继承]
-  // getUTCMinutes [继承]
-  // getUTCSeconds [继承]
-  // getUTCMilliseconds [继承]
-
-  /**
-   * [定制] set 系列方法。定制成返回 this 便于链式操作
+   * set 系列方法：定制成返回 this 便于链式操作
    */
   setTime() {
     Date.prototype.setTime.apply(this, arguments);
@@ -352,7 +150,6 @@ export class _Date extends Date {
   /**
    * 转换系列方法：转换成原始值和其他类型
    */
-  // [新增]
   [Symbol.toPrimitive](hint) {
     if (hint === 'number') {
       return this.toNumber();
@@ -361,47 +158,103 @@ export class _Date extends Date {
       return this.toString();
     }
   }
-  // [新增]
   toNumber() {
     return this.getTime();
   }
-  // [定制]
   toString(format = 'YYYY-MM-DD HH:mm:ss') {
     if (!this.toBoolean()) {
       return '';
     }
-    return format.replaceAll(this.constructor.REGEX_FORMAT, (match, $1) => {
+    // 格式化字符串用。总体同 element 用的 day.js 格式(https://day.js.org/docs/zh-CN/display/format)，风格定制成中文
+    const $this = this;
+    const formatMap = {
+      get YY() {
+        return `${$this.year}`.slice(-2);
+      },
+      get YYYY() {
+        return `${$this.year}`;
+      },
+      get M() {
+        return `${$this.month}`;
+      },
+      get MM() {
+        return `${$this.month}`.padStart(2, '0');
+      },
+      get D() {
+        return `${$this.day}`;
+      },
+      get DD() {
+        return `${$this.day}`.padStart(2, '0');
+      },
+      get d() {
+        return ['周日', '周一', '周二', '周三', '周四', '周五', '周六'][$this.week];
+      },
+      get dd() {
+        return ['星期日', '星期一', '星期二', '星期三', '星期四', '星期五', '星期六'][$this.week];
+      },
+      get H() {
+        return `${$this.hour}`;
+      },
+      get HH() {
+        return `${$this.hour}`.padStart(2, '0');
+      },
+      get h() {
+        return `${$this.shortHour}`;
+      },
+      get hh() {
+        return `${$this.shortHour}`.padStart(2, '0');
+      },
+      get m() {
+        return `${$this.minute}`;
+      },
+      get mm() {
+        return `${$this.minute}`.padStart(2, '0');
+      },
+      get s() {
+        return `${$this.second}`;
+      },
+      get ss() {
+        return `${$this.second}`.padStart(2, '0');
+      },
+      get SSS() {
+        return `${$this.millisecond}`.padStart(3, '0');
+      },
+      get a() {
+        return $this.hour < 12 ? '上午' : '下午';
+      },
+      get A() {
+        return this.a;
+      },
+      get Z() {
+        const sign = $this.timeZoneOffsetHour < 0 ? '+' : '-';
+        return `${sign}${(-$this.timeZoneOffsetHour).toString().padStart(2, '0')}:00`;
+      },
+      get ZZ() {
+        return this.Z.replace(':', '');
+      },
+    };
+    const rule = /\[([^\]]+)]|Y{1,4}|M{1,4}|D{1,2}|d{1,4}|H{1,2}|h{1,2}|a|A|m{1,2}|s{1,2}|Z{1,2}|SSS/g;
+    return format.replaceAll(rule, (match, $1) => {
       // [] 里面的内容原样输出
       if ($1) {
         return $1;
       }
       // 格式
-      if (match in this.format) {
-        return this.format[match];
+      if (match in formatMap) {
+        return formatMap[match];
       }
     });
   }
-  // [定制]
   toDateString(format = 'YYYY-MM-DD') {
     return this.toString(format);
   }
-  // [定制]
   toTimeString(format = 'HH:mm:ss') {
     return this.toString(format);
   }
-  // toLocaleString [继承]
-  // toLocaleDateString [继承]
-  // toLocaleTimeString [继承]
-  // toISOString [继承]
-  // toUTCString [继承]
-  // toGMTString [继承]
-  // [新增]
   toBoolean() {
     return !Number.isNaN(this.getTime());
   }
-  // [新增]
   toJSON(options = {}) {
     return this.toString();
   }
-  // valueOf [继承]
 }
